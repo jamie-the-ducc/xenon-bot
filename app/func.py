98f1f598544d42013eadcc57a41050c03cd34c1e
Zceia <94324458+EnbyCosmog@users.noreply.github.com>
2022-02-24 16:05:41 -0700

    fixed "rickroll" bot reply

--- app/func.py
+++ app/func.py
@@ -16,7 +16,8 @@ INFO = f" {w}[{Fore.BLUE}>{w}]"
 
 
 reply_dict = {
-    "rick roll":"https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713",
+    "rickroll":"https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713",
+    "rick":"https://tenor.com/view/rickroll-roll-rick-never-gonna-give-you-up-never-gonna-gif-22954713",
     "info":"> I'm Xenon Bot! Coded by `[Jam!3]#4466`",
     "reply":"> I replied!",
     "invite":"> Add me to your server!\n> https://discord.com/api/oauth2/authorize?client_id=943021878367359016&permissions=8&scope=bot"
